f918e82b64a4a429d92257613714ecd7b0e8cae0
Ben <ben.jacobs@vanti.co.uk>
2016-02-10 13:40:00 +0000

    pass config in as a parameter to the class

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var config = require('./config.json');
 
 //set express parameters to render jade template
 app.set('views', './views');
@@ -23,4 +24,4 @@ http.listen(3000, function(){
     console.log('Server listening on port 3000');
 });
 
-new control(io);
+new control(io, config);
